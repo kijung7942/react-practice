c39f154fa0b0608e3ae173c6f9465c02de477855
kijung7942@naver.com <kijung7942@naver.com>
2023-03-28 15:38:58 +0900

    ## console.log 정리

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,6 @@ const ChartBar = (props) => {
 	let barFillHeight = "0%";
 
 	if (props.maxValue > 0) {
-		console.log(props.maxValue);
 		barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
 	}
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -10,7 +10,6 @@ const NewExpense = (props) => {
 			...enteredExpenseData,
 			id: Math.random().toString(),
 		};
-		console.log(expenseDate);
 
 		props.onSaveExpenseDate(expenseDate);
 	};
