a2b5aa874b60a588881e2f21e836a35dcf16ed0c
kijung7942 <kijung7942@naver.com>
2023-03-31 11:33:34 +0900

    ## 요약 > Context API
    ## 설명
    1. contextAPI 가 만능은 아니다
     -> 보통 props를 더 많이 사용하고 특정한 경우에 contextAPI를 사용해야할지를 고민해보자

--- sideeffect-reducer-context/src/App.js
+++ sideeffect-reducer-context/src/App.js
@@ -6,29 +6,31 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const loginHandler = (email, password) => {
-		// We should of course check email and password
-		// But it's just a dummy/ demo anyways
-		localStorage.setItem("isLoggedIn", "1");
-		setIsLoggedIn(true);
-	};
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
 
-	const logoutHandler = () => {
-		localStorage.removeItem("isLoggedIn");
-		setIsLoggedIn(false);
-	};
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
 
-	return (
-		<AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-			<MainHeader onLogout={logoutHandler} />
-			<main>
-				{!isLoggedIn && <Login onLogin={loginHandler} />}
-				{isLoggedIn && <Home onLogout={logoutHandler} />}
-			</main>
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
