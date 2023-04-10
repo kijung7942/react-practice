fc7b01235e8452c93d1fe91aef5050b135fc11c8
kijung7942@naver.com <kijung7942@naver.com>
2023-04-10 18:48:20 +0900

    ## 요약 > 실전 프로젝트
    # 설명
     - cart context를 사용하면서 부모에게서 click props를 받을 필요없이 mealItem 자체에서 컨텍스트를 사용
     - input의 현재 값을 가져오기 위해 ref 이용

--- food-order-app/src/Components/Meal/MealItem.js
+++ food-order-app/src/Components/Meal/MealItem.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../store/cart-context";
 
-const MealItem = ({ value, onClick }) => {
-  return (
-    <div className={classes.meal}>
-      <div>
-        <h3>{value.name}</h3>
-        <p className={classes.description}>{value.description}</p>
-        <p className={classes.price}>${value.price}</p>
-      </div>
-      <div>
-        <MealItemForm value={value} onClick={onClick}></MealItemForm>
-      </div>
-    </div>
-  );
+const MealItem = ({ value }) => {
+	const cartCtx = useContext(CartContext);
+
+	const addToCartHandler = (amount) => {
+		cartCtx.addItem({
+			id: value.id,
+			name: value.name,
+			amount: amount,
+			price: value.price,
+		});
+	};
+	return (
+		<li className={classes.meal}>
+			<div>
+				<h3>{value.name}</h3>
+				<p className={classes.description}>{value.description}</p>
+				<p className={classes.price}>${value.price}</p>
+			</div>
+			<div>
+				<MealItemForm
+					onAddToCart={addToCartHandler}
+					id={value.id}
+					value={value}
+				></MealItemForm>
+			</div>
+		</li>
+	);
 };
 
 export default MealItem;
--- food-order-app/src/Components/Meal/MealItemForm.js
+++ food-order-app/src/Components/Meal/MealItemForm.js
@@ -1,27 +1,49 @@
-import React, { useContext, useRef } from "react";
+import React, { useRef, useState } from "react";
 import Input from "../UI/Input/Input";
 
 import classes from "./MealItemForm.module.css";
-import DummyMeal from "../../store/dummy-meals";
 
 const MealItemForm = (props) => {
-  const mealCtx = useContext(DummyMeal);
-  const inputRef = useRef();
+	const [amountIsValid, setAmountIsValid] = useState(true);
+	const amountInputRef = useRef();
 
-  const addHandler = (event) => {
-    event.preventDefault();
-    mealCtx.addAmount(props.value.id, inputRef.current.getAmount());
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredAmount = amountInputRef.current.value;
+		const enteredAmountNumber = +enteredAmount;
 
-  };
+		if (
+			enteredAmount.trim().length === 0 ||
+			enteredAmountNumber < 1 ||
+			enteredAmountNumber > 5
+		) {
+			setAmountIsValid(false);
+			return;
+		} 
+		setAmountIsValid(true);
+		props.onAddToCart(enteredAmountNumber);
+	};
 
-  return (
-    <div>
-      <form className={classes.form} onSubmit={addHandler}>
-        <Input ref={inputRef} onChange={mealCtx.addHandler}></Input>
-        <button>+ Add</button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form className={classes.form} onSubmit={submitHandler}>
+				<Input
+					ref={amountInputRef}
+					label="Amount"
+					input={{
+						id: "amount" + props.value.id,
+						type: "number",
+						min: "1",
+						max: "5",
+						step: "1",
+						defaultValue: "1",
+					}}
+				></Input>
+				<button>+ Add</button>
+				{!amountIsValid && <p>Please enter a valid amoint (1-5).</p>}
+			</form>
+		</div>
+	);
 };
 
 export default MealItemForm;
