d4987dbfedc76eb29c8049a367fee052fbec8150
kijung7942@naver.com <kijung7942@naver.com>
2023-04-11 10:59:14 +0900

    ## 요약 > 실전 프로젝트
    # 설명
     - 장바구니에 item이 추가될 때 버튼의 class를 변경하여 애니메이션 추가
     - useEffect를 이용하여 cart context의 items의 변화에만 반응하도록 하고
    items의 길이에 따라  boolean 상태를 변화시키는 useState를 활용함.
     - 두 번째 이상의 useEffect에서는 타이머를 삭제한 후 다시 적용(중첩방지)하기 위해
    useEffect에서 클린업 함수를 return 함.

--- food-order-app/src/Components/UI/Header/HeaderCartButton.js
+++ food-order-app/src/Components/UI/Header/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
 	const cartCtx = useContext(CartContext);
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
 	const numberOfCartItems = cartCtx.items.reduce((acc, cur) => {
-		return acc = acc + cur.amount;
+		return (acc = acc + cur.amount);
 	}, 0);
 
+	const btnClasses = `${classes.button} ${
+		btnIsHighlighted ? classes.bump : ""
+	}`;
+
+	useEffect(() => {
+		if (cartCtx.items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [cartCtx.items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span>
 				<CartIcon className={classes.icon} />
 			</span>
