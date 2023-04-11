3ca148d96c2904cf83467aa0f84074407e681000
kijung7942@naver.com <kijung7942@naver.com>
2023-04-11 13:48:03 +0900

    ## 요약 > React의 상태 벼노하에 따른 렌더링 이해하기
    ## 설명
     1. React는 DOM과는 관련없다.
      -> react는 state, context, effect 등을 이용하여 데이터의 상태를 관리함
      -> rendering은 React DOM이 함(Virtual DOM과 Real DOM을 비교하여 변경된 부분만 렌더링)
      2. 자식컴포넌트는 부모컴포넌트의 상태가 변경되면 재평가 & 재실행 된다.

---
+++ component-update/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+import Button from './components/UI/Button/Button';
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+
+function App() {
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	console.log("App Running")
+
+	const toggleParagraphHandler = () => {
+		setShowParagraph(state => !state);
+	}
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+			<DemoOutput show={showParagraph}></DemoOutput>
+			<Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
