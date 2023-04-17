55eda1f4c11af3ebad3a77da4cd8833a901be39c
kijung7942@naver.com <kijung7942@naver.com>
2023-04-17 14:49:15 +0900

    ## 요약 > useContext를 대체하는 static contextType
    ## 설명
     1. 클래스형 컴포넌트에서는 useContext를 사용할 수 없음
      -> context 값에 접근하기 위해서 static으로 선언한 contextType을 명시해야함.
     2. 한 컴포넌트에서 여러개의 Context에 접근 어려움
      -> 꼭 해야하는 경우 여러겹의 컴포넌트로 래핑해야함.

--- class-component/src/components/UserFinder.js
+++ class-component/src/components/UserFinder.js
@@ -1,31 +1,31 @@
 import { Fragment, Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
+import UsersContext from "../store/users-context";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
 
   componentDidMount() {
+    console.log(this.context)
     // HTTP 요청 등.. (useEffect에 빈배열을 의존성으로 준 경우와 같음.)
-    this.setState({filteredUsers: DUMMY_USERS});
+    this.setState({filteredUsers: this.context.users});
   }
 
   componentDidUpdate(prevProps, prevState) {
     // useEffect에 seachTerm을 포함한 의존성 배열을 준 경우와 같음.
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -42,10 +42,10 @@ class UserFinder extends Component {
   render() {
     return (
       <Fragment>
-        <div className={classes.finder}>
-          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
-        </div>
-        <Users users={this.state.filteredUsers} />
+          <div className={classes.finder}>
+            <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+          </div>
+          <Users users={this.state.filteredUsers} />
       </Fragment>
     );
   }
