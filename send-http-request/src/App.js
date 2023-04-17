e4b7b4c1d1445242242b3cbbdabc20b677f9217c
kijung7942@naver.com <kijung7942@naver.com>
2023-04-17 16:46:14 +0900

    ## 요약 > http요청 보내기(db연결)
    ## 설명
     1. useEffect를 이용해 컴포넌트가 처음 마운트 되는 순간에 data를 fetching 해오도록 변경
     2. useCallback을 이용해 함수를 저장함(여기서는 안해도 됨)
      -> 외부에서 주어지는 state값 등을 사용하는 경우에 useCallback 으로 감싸줘야 함.

--- send-http-request/src/App.js
+++ send-http-request/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,17 +8,17 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
-  
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -28,18 +28,21 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-    } catch(error) {
+    } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
 
-  let content = <p>Found no movies.</p>
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  let content = <p>Found no movies.</p>;
 
-  if(movies.length > 0) {
-    content = <MoviesList movies={movies}/>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
-  if(error) {
+  if (error) {
     content = <p>{error}</p>;
   }
   if (isLoading) {
@@ -51,9 +54,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
