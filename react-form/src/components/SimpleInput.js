4e6d4c3eec8e4465697ecf57efb2942b6df3219b
kijung7942@naver.com <kijung7942@naver.com>
2023-04-20 13:13:12 +0900

    ## 요약 > React의 Form 알아보기
    ## 설명
     1. 폼 안의 인풋에서 데이터를 꺼내서 조작(manipulate) 하는 방법
      -> state 사용, ref 사용
      * 데이터 조작을 위해서는 state를 사용하는 것이 이상적임.

--- react-form/src/components/SimpleInput.js
+++ react-form/src/components/SimpleInput.js
@@ -1,15 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const [enteredName, setEnteredName] = useState("");
+	 const nameInput = useRef();
+
+	const nameInputChangeHandler = (event) => {
+		setEnteredName(event.target.value);
+	};
+
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+		console.log(enteredName);
+
+		const enteredValue = nameInput.current.value;
+		console.log(enteredValue);
+
+		// nameInput.current.value = ''; // 리액트를 사용할때 좋은 방법이 아님. 
+		setEnteredName('');
+	}
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className="form-control">
+				<label htmlFor="name">Your Name</label>
+				<input ref={nameInput} value={enteredName} type="text" id="name" onChange={nameInputChangeHandler} />
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
