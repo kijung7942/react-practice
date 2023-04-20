a4fc20ec58ba83179a6a2f6d1734eb41cea464f5
kijung7942@naver.com <kijung7942@naver.com>
2023-04-20 18:54:25 +0900

    ## 요약 > React의 Form 알아보기
    ## 설명
     1. form의 submit 를 이용하여 제출되는 순간 invalidation(event.preventDefault 해주기)
     2. 인풋의 onChange와 onBlur를 이용하여 변경 감지하는 순간 invalidation

--- react-form/src/components/SimpleInput.js
+++ react-form/src/components/SimpleInput.js
@@ -1,8 +1,16 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+	const nameInput = useRef();
 	const [enteredName, setEnteredName] = useState("");
-	 const nameInput = useRef();
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+	useEffect(() => {
+		if(enteredNameIsValid) {
+			console.log("name input is Valid")
+		}
+	}, [enteredNameIsValid])
 
 	const nameInputChangeHandler = (event) => {
 		setEnteredName(event.target.value);
@@ -10,20 +18,49 @@ const SimpleInput = (props) => {
 
 	const formSubmitHandler = (event) => {
 		event.preventDefault();
+
+		setEnteredNameTouched(true);
+
+		if (!enteredName.trim()) {
+			setEnteredNameIsValid(false);
+			return;
+		}
+		setEnteredNameIsValid(true);
 		console.log(enteredName);
 
 		const enteredValue = nameInput.current.value;
 		console.log(enteredValue);
 
-		// nameInput.current.value = ''; // 리액트를 사용할때 좋은 방법이 아님. 
-		setEnteredName('');
+		// nameInput.current.value = ''; // 리액트를 사용할때 좋은 방법이 아님.
+		setEnteredName("");
+	};
+
+	const nameInputBlurHandler = (event) => {
+		setEnteredNameTouched(true);
+
+		if (!enteredName.trim()) {
+			setEnteredNameIsValid(false);
+			return;
+		}
+		
 	}
 
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+	const nameInputClasses = nameInputIsInvalid ? 'form-control invalid': 'form-control'
 	return (
-		<form onSubmit={formSubmitHandler}>
+		<form onSubmit={formSubmitHandler} className={nameInputClasses}>
 			<div className="form-control">
 				<label htmlFor="name">Your Name</label>
-				<input ref={nameInput} value={enteredName} type="text" id="name" onChange={nameInputChangeHandler} />
+				<input
+					ref={nameInput}
+					value={enteredName}
+					type="text"
+					id="name"
+					onChange={nameInputChangeHandler}
+					onBlur={nameInputBlurHandler}
+				/>
+				{nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
