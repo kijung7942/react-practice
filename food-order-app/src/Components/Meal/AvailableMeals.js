7b5c9de954760fdc97facf912e18856ec38629e3
kijung7942@naver.com <kijung7942@naver.com>
2023-05-02 14:16:05 +0900

    ## 요약 > food-order-app에 http 및 form 적용
    ## 새로운 기능
     1. meals list 불러오기
     -> fetch api 를 이용해서 목록 불러오기
     2. async 함수 내부에서 try-catch 구문 불가
     -> 반환되는 promise에서 catch로 내부의 예외를 잡아낼 수 있음.

--- food-order-app/src/Components/Meal/AvailableMeals.js
+++ food-order-app/src/Components/Meal/AvailableMeals.js
@@ -1,52 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-    amount: 0,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-    amount: 0,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-    amount: 0,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-    amount: 0,
-  },
-];
-
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [httpError, setHttpError] = useState(null);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-h-c800f-default-rtdb.firebaseio.com/meals.json"
+      );
 
-	const mealsList = DUMMY_MEALS.map((meal) => {
-		return <MealItem key={meal.id} value={meal}></MealItem>;
-	})
+      if (!response.ok) {
+        throw new Error("somthing went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeal = [];
+
+      for (const key in responseData) {
+        loadedMeal.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeal);
+    };
+
+    fetchMeals().catch((error) => {
+      setHttpError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => {
+    return <MealItem key={meal.id} value={meal}></MealItem>;
+  });
 
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>
-          {mealsList}
-        </ul>
-      </Card>
+      {httpError ? (
+        <p className={classes.MealIsLoading}>{httpError}</p>
+      ) : meals.length === 0 ? (
+        <p className={classes.MealIsLoading}>Loading...</p>
+      ) : (
+        <Card>
+          <ul>{mealsList}</ul>
+        </Card>
+      )}
     </section>
   );
 };
