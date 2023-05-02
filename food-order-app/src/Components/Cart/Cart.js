f497621b2439674dd7bb0b752c785af4f573b653
kijung7942@naver.com <kijung7942@naver.com>
2023-05-02 15:33:52 +0900

    ## 요약 > food-order-app에 http 및 form 적용
    ## 새로운 기능
     1. form 제출 상태에 따라 모달에 상태 표시하여 한층 나은 ux를 제공
     2. Cart Provider에 새로운 기능 CLEAR(Cart 비우기) 추가하여 order 후에 Cart를 비우도록 구현

--- food-order-app/src/Components/Cart/Cart.js
+++ food-order-app/src/Components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import Modal from "../UI/Modal/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
@@ -7,6 +7,9 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidsSubmit] = useState(false);
+
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -19,6 +22,20 @@ const Cart = (props) => {
 
   const orderHandler = () => {
     setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    const response = await fetch(
+      "https://react-h-c800f-default-rtdb.firebaseio.com//orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({ user: userData, orderedITem: cartCtx.items }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidsSubmit(true);
+	cartCtx.clearCart();
   };
 
   const cartItems = (
@@ -53,13 +70,33 @@ const Cart = (props) => {
 
   return (
     <Modal onClose={props.onHideCart}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      {isCheckout && <Checkout onCancel={props.onHideCart} />}
-      {!isCheckout && modalActions}
+      {!isSubmitting && !didSubmit && (
+        <Fragment>
+          {cartItems}
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>{totalAmount}</span>
+          </div>
+          {isCheckout && (
+            <Checkout
+              onConfirm={submitOrderHandler}
+              onCancel={props.onHideCart}
+            />
+          )}
+          {!isCheckout && modalActions}
+        </Fragment>
+      )}
+      {isSubmitting && <p>Sending order data...</p>}
+      {didSubmit && !isSubmitting && (
+        <Fragment>
+          <p>Successfully sent the order!</p>
+          <div className={classes.actions}>
+            <button className={classes.button} onClick={props.onHideCart}>
+              Close
+            </button>
+          </div>
+        </Fragment>
+      )}
     </Modal>
   );
 };
