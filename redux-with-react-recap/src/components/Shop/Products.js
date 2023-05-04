092c76b6ccd00784d87d694759951cb4af534d94
kijung7942@naver.com <kijung7942@naver.com>
2023-05-04 17:18:57 +0900

    ## 요약 > 상품 더미데이터 추가
    ## 기타
     1. 상품 더미데이터 추가 및 렌더링

--- redux-with-react-recap/src/components/Shop/Products.js
+++ redux-with-react-recap/src/components/Shop/Products.js
@@ -1,16 +1,32 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCT = [
+  {
+    id: 1,
+    title: "My first Book",
+    price: 5,
+    description: "the first book i ever wrote",
+  },
+  {
+    id: 2,
+    title: "My second Book",
+    price: 6,
+    description: "the second book i ever wrote",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCT.map((item) => (
+          <ProductItem
+          key={item.id}
+          item={item}
+          />
+        ))}
       </ul>
     </section>
   );
