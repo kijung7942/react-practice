305f47c0fcee14d56bc179bb88abf0112499cffb
kijung7942@naver.com <kijung7942@naver.com>
2023-05-04 16:11:33 +0900

    ## 요약 > 장바구니 ui 토글 기능 추가
    ## 새로운 기능
     1. Header에 달린 cartButton을 토글로 하여 Cart Component를 숨겼다가 보였다가 하는 기능 추가

--- redux-with-react-recap/src/components/Cart/CartButton.js
+++ redux-with-react-recap/src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartButton.module.css';
+import { uiAction } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  
+  const cartButtonClickHandler = () => {
+    dispatch(uiAction.toggle());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartButtonClickHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
