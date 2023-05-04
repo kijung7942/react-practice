32d573c5b02367d2531aee3b3c8af9583adda6c3
kijung7942@naver.com <kijung7942@naver.com>
2023-05-04 17:21:24 +0900

    ## 요약 > 장바구니 기능 추가
    ## 새로운 기능
     1. CartButton에 장바구니에 담긴 상품의 총 수량 표시
     2. 장바구니에 담긴 항목 리스트 출력
     3. 장바구니 리스트에서 항목 추가 혹은 제거

--- redux-with-react-recap/src/components/Cart/CartButton.js
+++ redux-with-react-recap/src/components/Cart/CartButton.js
@@ -1,18 +1,23 @@
-import { useDispatch } from 'react-redux';
-import classes from './CartButton.module.css';
-import { uiAction } from '../../store/ui-slice';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiAction } from "../../store/ui-slice";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-  
+  const items = useSelector((state) =>
+    state.cart.items.reduce((acc, cur) => {
+      return acc + cur.quantity;
+    }, 0)
+  );
+
   const cartButtonClickHandler = () => {
     dispatch(uiAction.toggle());
-  }
+  };
 
   return (
     <button className={classes.button} onClick={cartButtonClickHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{items}</span>
     </button>
   );
 };
--- redux-with-react-recap/src/components/Cart/CartItem.js
+++ redux-with-react-recap/src/components/Cart/CartItem.js
@@ -1,8 +1,20 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartAction } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
-
+  const { id, title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const removeHandler = () => {
+    dispatch(cartAction.removeItemFromCart(id))
+  }
+  const addHandler = () => {
+    dispatch(cartAction.addItemToCart({
+      id,
+      title,
+      price
+    }))
+  }
   return (
     <li className={classes.item}>
       <header>
@@ -17,8 +29,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeHandler}>-</button>
+          <button onClick={addHandler}>+</button>
         </div>
       </div>
     </li>
