32191f45d7293a667d45f8cb148f80377672f0ae
kijung7942@naver.com <kijung7942@naver.com>
2023-05-18 15:24:39 +0900

    ## 요약 > Link에 스타일 입히기
    ## 설명
     1. 일반 Link 컴포넌트 에서는 상태에 따른 스타일을 입히기 어렵기 때문에 NavLink로 변경해준다.
     2. className에서 받아오는 파라미터 객체에서 isActive(boolean) 값을 기준으로 className을 설정

--- react-router/src/components/MainNavigation.js
+++ react-router/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from './MainNavigation.module.css'
 
@@ -7,10 +7,10 @@ const MainNavigation = () => {
         <nav>
             <ul className={classes.list}>
                 <li>
-                    <Link to="/">Home</Link>
+                    <NavLink to="/" className={({isActive}) => isActive &&classes.active}>Home</NavLink>
                 </li>
                 <li>
-                    <Link to="/products">Products</Link>
+                    <NavLink to="/products" className={({isActive}) => isActive &&classes.active}>Products</NavLink>
                 </li>
             </ul>
         </nav>
