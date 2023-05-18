91435b9f1ccb0ac09f84ec4f352363128f443a01
kijung7942@naver.com <kijung7942@naver.com>
2023-05-18 17:36:20 +0900

    ## 요약 > router 활용(플레이스홀더,상대경로와 절대경로)
    ## 설명
     1. path 값을 줄 때 콜론(:)으로 명시하면 그 값은 동적으로 사용이 가능한 플레이스홀더가 됨.
     2. 플레이스 홀더의 값은 react-router-dom이 제공하는 useParams hook을 이용해서 꺼내 쓸 수 있음.
     3. path 지정 시 / 로 시작하면 절대경로임.
     4. Link 컴포넌트의 to props에 .. 을 이용해서 상위로 이동가능
     -> 이때, relative props를 이용해 path 혹은 router 상에서 한 단계 상위로 갈지 결정 가능함.

---
+++ react-router/src/pages/ProductDetail.js
@@ -0,0 +1,12 @@
+import { Link, useParams } from "react-router-dom"
+
+const ProductDetailPage = () => {
+    const params = useParams();
+    return <>
+        <h1>Product Detail</h1>
+        <p>{params.productId}</p>
+        <Link to=".." relative="path">Back</Link>
+    </>
+}
+
+export default ProductDetailPage
