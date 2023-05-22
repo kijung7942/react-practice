15eaae1da5ab70d1c377f6cf01e283842036cf97
kijung7942@naver.com <kijung7942@naver.com>
2023-05-22 16:52:49 +0900

    ## 요약 > defer를 이용한 부분 렌더링
    ## 설명
     1. loader에 defer 함수로 감싼 내용을 return함으로써 데이터 로딩이 완전하지 않더라도 일부 렌더링을 시작
     2. 여러개의 http 요청이 필요한 페이지에서 유용함.

--- react-spa-app/frontend/src/pages/Events.js
+++ react-spa-app/frontend/src/pages/Events.js
@@ -1,31 +1,37 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  // if (data.isError) {
-  // return <p>{data.message}</p>
-  // }
-
-  const {events} = data;
   return (
-    <EventsList events={events} />
-  );
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  )
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.'}
     // throw new Response(JSON.stringify({ message: 'Could not fetch events' }), { status: 500 })
     throw json({ message: 'Could not fetch events' }, { status: 500 })
   } else {
-    // const resData = await response.json();
-    // return resData.events;
-    return response;
+    const resData = await response.json(); // defer를 사용하면 직접 파싱을 해야함.
+    return resData.events;
+    // return response;
   }
+}
+
+export async function loader() {
+  return defer({
+    events: loadEvents()
+  })
 }
