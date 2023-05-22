9a52ec4c6a5f24ab9bb567d220ed0d81199a87ca
kijung7942@naver.com <kijung7942@naver.com>
2023-05-22 14:52:56 +0900

    ## 요약 > 상태에 따른 UI 변경
    ## 설명
    1. useNavigation을 이용해 얻어낸 객체에서 state 프로퍼티에 따라 UI 업데이트 가능
     -> state가 submitting 이면 제출 중 으로 표시 및 disabled 활성화 등

--- react-spa-app/frontend/src/components/EventForm.js
+++ react-spa-app/frontend/src/components/EventForm.js
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting'
+
   function cancelHandler() {
     navigate('..');
   }
@@ -16,21 +20,23 @@ function EventForm({ method, event }) {
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''}/>
+        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''} />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''}/>
+        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''} />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''}/>
+        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''} />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...': 'Save'}
+        </button>
       </div>
     </Form>
   );
